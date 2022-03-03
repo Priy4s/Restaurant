364220d065335e897ad85ab61896d9f18761398e
Priy4s <94791821+Priy4s@users.noreply.github.com>
2022-03-03 12:42:03 +0100

    aanpassing menu

--- Menukaart/Menukaart/Program.cs
+++ Menukaart/Menukaart/Program.cs
@@ -8,6 +8,7 @@ namespace Menukaart
         {
             while (true)
             {
+                Console.WriteLine("---------------------------------------------------------------------");
                 Console.WriteLine("\n Menukaart\n");
                 Console.WriteLine("1. Brunch");
                 Console.WriteLine("2. Lunch");
@@ -21,6 +22,7 @@ namespace Menukaart
 
                 if (keuze == 1)
                 {
+                    Console.WriteLine("---------------------------------------------------------------------");
                     Console.WriteLine("\nBrunch");
                     Console.WriteLine(" Pannenkoek galette met ei - 7,50");
                     Console.WriteLine(" Rugelach recept met jam - 6,95 (G)");
@@ -30,6 +32,7 @@ namespace Menukaart
                 }
                 else if (keuze == 2)
                 {
+                    Console.WriteLine("---------------------------------------------------------------------");
                     Console.WriteLine("\nLunch");
                     Console.WriteLine(" Franse frietjes uit de oven met verse kruiden - 4,50 (V)");
                     Console.WriteLine(" Champignonsalade met spinazie - 4,95 (V,G)");
@@ -44,6 +47,7 @@ namespace Menukaart
                 }
                 else if (keuze == 3)
                 {
+                    Console.WriteLine("---------------------------------------------------------------------");
                     Console.WriteLine("\n Diner");
                     Console.WriteLine("1. Voorgerechten");
                     Console.WriteLine("2. Hoofdgerechten");
@@ -56,6 +60,7 @@ namespace Menukaart
                     {
 
                         //Voorgerechten
+                        Console.WriteLine("---------------------------------------------------------------------");
                         Console.WriteLine("\nVoorgerechten:  ");
                         Console.WriteLine(" Caesar salade met kip en bacon - 8,95");
                         Console.WriteLine(" Gebakken uien dip - 5,95");
@@ -70,6 +75,7 @@ namespace Menukaart
                     }
                     else if (keuze == 2)
                     {
+                        Console.WriteLine("---------------------------------------------------------------------");
                         Console.WriteLine("\nHoofdgerechten:");
                         Console.WriteLine(" -Sushi (8 stuks): ");
                         Console.WriteLine("   Sushi garnaal - 12,50 ");
@@ -87,6 +93,7 @@ namespace Menukaart
                     //Nagerechten
                     else if (keuze == 3)
                     {
+                        Console.WriteLine("---------------------------------------------------------------------");
                         Console.WriteLine("\nNagerechten:");
                         Console.WriteLine(" Pavlova met rode sinaasappel - 5,95 ");
                         Console.WriteLine(" Dame Blanche klassiek - 6,50 ");
@@ -100,39 +107,54 @@ namespace Menukaart
 
                 else if (keuze == 4)
                 {
+                    Console.WriteLine("---------------------------------------------------------------------");
                     Console.WriteLine("\n Drinken\n");
+                    Console.WriteLine("1. Non alcoholische dranken");
+                    Console.WriteLine("2. Alcoholische dranken (A)");
+                    Console.WriteLine("0. Terug");
+                    input = Console.ReadLine();
+                    keuze = Convert.ToInt32(input);
                     //Warme dranken
-                    Console.WriteLine("Warme dranken\n");
-                    Console.WriteLine(" Zwarte koffie - 3.50");
-                    Console.WriteLine(" Cappuccino - 3.70");
-                    Console.WriteLine(" Koffie verkeerd - 3.50");
-                    Console.WriteLine(" Munt thee - 3.00");
-                    Console.WriteLine(" Gember thee - 3.10\n");
-                    //Koude dranken
-                    Console.WriteLine("Koude dranken\n");
-                    Console.WriteLine(" Coca Cola (regular/light/zero) - 2.90");
-                    Console.WriteLine(" Fanta/Sprite - 2.90");
-                    Console.WriteLine(" Water (plat/bruisend) - 2.70");
-                    Console.WriteLine(" Lipton Ice Tea Sparkling of Green - 2.75");
-                    Console.WriteLine(" Verse Jus d'Orange - 3.15\n");
+                    if (keuze == 1)
+                    {
+                        Console.WriteLine("---------------------------------------------------------------------");
+                        Console.WriteLine("\nNon alcoholische dranken\n");
+                        Console.WriteLine(" -Warme dranken");
+                        Console.WriteLine("   Zwarte koffie - 3.50");
+                        Console.WriteLine("   Cappuccino - 3.70");
+                        Console.WriteLine("   Koffie verkeerd - 3.50");
+                        Console.WriteLine("   Munt thee - 3.00");
+                        Console.WriteLine("   Gember thee - 3.10\n");
+                        //Koude dranken
+                        Console.WriteLine(" -Koude dranken");
+                        Console.WriteLine("   Coca Cola (regular/light/zero) - 2.90");
+                        Console.WriteLine("   Fanta/Sprite - 2.90");
+                        Console.WriteLine("   Water (plat/bruisend) - 2.70");
+                        Console.WriteLine("   Lipton Ice Tea Sparkling of Green - 2.75");
+                        Console.WriteLine("   Verse Jus d'Orange - 3.15\n");
+                    }
                     //Alcoholische dranken
-                    Console.WriteLine("Alcoholische dranken\n");
-                    Console.WriteLine(" -Bier:");
-                    Console.WriteLine("   Jupiler - 2.60");
-                    Console.WriteLine("   Heineken - 3.25");
-                    Console.WriteLine("   Hertog Jan - 3.10");
-                    Console.WriteLine(" -Wijn:");
-                    Console.WriteLine("   Rosé/zoete witte wijn - 3.50");
-                    Console.WriteLine("   Merlot wijn - 3.60");
-                    Console.WriteLine("   Rode wijn - 3.10");
-                    Console.WriteLine(" -Cocktails:");
-                    Console.WriteLine("   Passion Energy - 9.75");
-                    Console.WriteLine("   Red ginger - 9.70");
-                    Console.WriteLine("   Mojito (regular/strawberry mojito) - 9.70");
-
+                    else if (keuze == 2)
+                    {
+                        Console.WriteLine("---------------------------------------------------------------------");
+                        Console.WriteLine("\nAlcoholische dranken\n");
+                        Console.WriteLine(" -Bier:");
+                        Console.WriteLine("   Jupiler - 2.60");
+                        Console.WriteLine("   Heineken - 3.25");
+                        Console.WriteLine("   Hertog Jan - 3.10");
+                        Console.WriteLine(" -Wijn:");
+                        Console.WriteLine("   Rosé/zoete witte wijn - 3.50");
+                        Console.WriteLine("   Merlot wijn - 3.60");
+                        Console.WriteLine("   Rode wijn - 3.10");
+                        Console.WriteLine(" -Cocktails:");
+                        Console.WriteLine("   Passion Energy - 9.75");
+                        Console.WriteLine("   Red ginger - 9.70");
+                        Console.WriteLine("   Mojito (regular/strawberry mojito) - 9.70");
+                    }
                 }
                 else if (keuze == 5)
                 {
+                    Console.WriteLine("---------------------------------------------------------------------");
                     Console.WriteLine("\n Alergieën informatie\n");
                     Console.WriteLine("V - Vegan");
                     Console.WriteLine("N - Bevat noten");
