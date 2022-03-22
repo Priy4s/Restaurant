66a116133abde5b376f456741b6bf47405ca9603
SherynaK <99250047+SherynaK@users.noreply.github.com>
2022-03-22 11:26:56 +0100

    Add files via upload

---
+++ menuJson/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace menujson
+{
+    class Menuinfo
+    {
+        public string Naam { get; set; }
+        public double Prijs { get; set; }
+        public List<string> Allergie { get; set; }
+        public string Categorie { get; set; }
+
+    }
+
+    class Menu
+    {
+        public static void Main(string[] args)
+        {
+            string menures = String.Empty;
+            menures = File.ReadAllText(@"menu.json");
+            JsonConvert.DeserializeObject<Menuinfo>(menures);
+
+
+        }
+    }
+}
+
